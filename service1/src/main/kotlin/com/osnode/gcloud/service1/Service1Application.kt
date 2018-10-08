277df943163b6706bce89e15e06f3cac00c27a42
Geoffrey Rekier <geoffrey.rekier@gjensidige.no>
2018-10-08 14:51:20 +0200

    Went too fast and one class was not saved yet

--- service1/src/main/kotlin/com/osnode/gcloud/service1/Service1Application.kt
+++ service1/src/main/kotlin/com/osnode/gcloud/service1/Service1Application.kt
@@ -2,10 +2,12 @@ package com.osnode.gcloud.service1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 class Service1Application
 
 fun main(args: Array<String>) {
